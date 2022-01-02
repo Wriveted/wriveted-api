be2f687c9b195d28b5a4d65a64777325216a062b
Brian Thorne <brian@thorne.link>
2022-01-03 10:17:40 +1300

    💡 Document collection update API

--- app/api/collections.py
+++ app/api/collections.py
@@ -73,11 +73,45 @@ async def update_school_collection(
 ):
     """
     Update a school library collection with a list of changes.
+
+    Changes can be to add, remove, or update editions of books. Many
+    changes of different types can be added in a single call to this
+    API, they must however all refer to one school.
+
+    If the edition are already in the Wriveted database only the ISBN
+    and `"action"` is required. For example adding known editions to a
+    collection can be achieved with entries of this form:
+
+    ```json
+    { "ISBN": "XYZ", "action": "add" }
+    ```
+
+    Note any unknown editions will not get included in the update, a list of ISBNs
+    that were skipped can be found in the response.
+
+    ### Action Types
+
+    - `add`
+    - `remove`
+    - `update` - still need to work this one out
+
+    ### Adding an unknown work to a collection
+
+    In the case where an edition is not yet in the Wriveted database some more
+    information can be provided in the update to automatically add the edition.
+    Alternatively, missing Works and Editions can be created using their direct
+    endpoints.
+
+    The optional key `edition_info` can be provided for each addition, with the
+    same format as the `POST /edition` endpoint.
+
+
     """
     logger.info("Updating collection for school", school=school, account=account)
 
     isbns_to_remove = []
     editions_to_add: List[EditionCreateIn] = []
+    skipped_edditions: List[str] = []
 
     for update in collection_update_data:
         if update.action == CollectionUpdateType.REMOVE:
@@ -86,10 +120,16 @@ async def update_school_collection(
             if update.edition_info is None:
                 # this is a bit hacky
                 update.edition_info = EditionCreateIn.parse_obj(crud.edition.get(session, id=update.ISBN))
-            logger.info("Edition to add", new_edition=update.edition_info)
-            editions_to_add.append(update.edition_info)
+
+            if update.edition_info is None:
+                # If update.edition_info is still None then the caller didn't give us information, and we don't
+                # have this edition in the database. We will skip and report this to the caller.
+                skipped_edditions.append(update.ISBN)
+            else:
+                logger.info("Edition to add", new_edition=update.edition_info)
+                editions_to_add.append(update.edition_info)
         else:
-            raise NotImplemented("TODO...")
+            raise NotImplemented("TODO, work out what updating a collection looks like...")
 
     if len(isbns_to_remove) > 0:
         logger.info(f"Removing {len(isbns_to_remove)} items from collection")
@@ -112,6 +152,8 @@ async def update_school_collection(
 
         await add_editions_to_collection(session, editions_to_add, school, account)
 
+
     return {
-        'msg': "updated"
+        'msg': "updated",
+        "skipped": skipped_edditions
     }
