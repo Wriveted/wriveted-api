ebbec7be306edc8f8dd1aee4c8053666bbe28703
Brian Thorne <brian@thorne.link>
2022-01-16 11:48:14 +1300

    💥 Creating events shouldn't always commit a DB transaction

--- app/services/events.py
+++ app/services/events.py
@@ -6,23 +6,27 @@ from app.models import Event, ServiceAccount, User, EventLevel
 
 
 def create_event(
-        session: Session,
-        title: str,
-        description: str,
-        level: EventLevel = EventLevel.NORMAL,
-        school=None,
-        account: Union[ServiceAccount, User] = None,
-
+    session: Session,
+    title: str,
+    description: str,
+    level: EventLevel = EventLevel.NORMAL,
+    school=None,
+    account: Union[ServiceAccount, User] = None,
+    commit: bool = True,
 ):
     user = account if isinstance(account, User) else None
     service_account = account if isinstance(account, ServiceAccount) else None
 
-    session.add(Event(
+    event = Event(
         title=title,
         description=description,
         level=level,
         school=school,
         user=user,
         service_account=service_account,
-    ))
-    session.commit()
+    )
+    session.add(event)
+    if commit:
+        session.commit()
+        session.refresh(event)
+    return event
