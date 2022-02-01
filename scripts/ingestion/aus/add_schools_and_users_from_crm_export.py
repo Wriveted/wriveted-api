43005a626b84f67b8ab2c709b23911b3e0bff43b
Brian Thorne <brian@thorne.link>
2022-02-01 21:58:00 +1300

    Script to import users from CRM export

--- scripts/ingestion/aus/add_schools_and_users_from_crm_export.py
+++ scripts/ingestion/aus/add_schools_and_users_from_crm_export.py
@@ -2,6 +2,8 @@
 # directly to the database. E.g. if running via docker-compose
 import os
 
+from app.models import SchoolState
+from app.models.user import UserAccountType
 from app.schemas.user import UserCreateIn
 
 os.environ['POSTGRESQL_SERVER'] = 'localhost/'
@@ -50,27 +52,28 @@ class Contact(pydantic.BaseModel):
     school_specific_email_domain: Optional[str]
     library_management_system: Optional[str]
     email: EmailStr
-    associated_company: str
+    associated_company: Optional[str]
 
 
 contacts = []
 # First try find all the schools.
 for line in csv_file:
+
     contact = Contact(
-        id=line[0],
-        school_official_id=line[1],
-        first_name=line[2],
-        last_name=line[3],
-        school_size=line[4] if len(line[4]) else None,
-        state_or_region=line[5],
-        school_name=line[6],
-        country=line[7],
-        job_title=line[8],
-        school_email_domain=line[9],
-        school_specific_email_domain=line[10],
-        library_management_system=line[11],
-        email=line[12],
-        associated_company=line[13],
+        id=line[0].strip(),
+        school_official_id=line[1] if len(line[1]) > 1 else None,
+        first_name=line[2].strip(),
+        last_name=line[3].strip(),
+        school_size=line[4] if len(line[4]) > 1 else None,
+        state_or_region=line[5].strip(),
+        school_name=line[6].strip(),
+        country=line[7].strip(),
+        job_title=line[8].strip() if len(line[8]) > 1 else None,
+        school_email_domain=line[9].strip(),
+        school_specific_email_domain=line[10].strip(),
+        library_management_system=line[11].strip(),
+        email=line[12].strip(),
+        associated_company=line[13].strip() if len(line[13]) > 1 else None,
 
     )
 
@@ -80,16 +83,21 @@ print(f"Loaded {len(contacts)} contacts from CRM export")
 
 # Add User accounts for all the contacts, include the CRM contact ID and job title.
 users = []
+
+unhandled_contacts = []
+
 for contact in contacts:
-    user = crud.user.create(
+    user = crud.user.get_or_create(
         db=session,
-        obj_in=UserCreateIn(
+        user_data=UserCreateIn(
             name=f"{contact.first_name.title()} {contact.last_name.title()}",
             email=contact.email,
-            info={"other": {
-                "job_title": contact.job_title,
-                "associated_company": contact.associated_company
-            }},
+            info={
+                "other": {
+                    "job_title": contact.job_title,
+                    "associated_company": contact.associated_company
+                }
+            },
         ),
         commit=False
     )
@@ -99,35 +107,49 @@ for contact in contacts:
     country_code = "AUS" if contact.country == "Australia" else "USA"
 
     if contact.school_official_id is not None:
-        school = crud.school.get_by_official_id_or_404(
-            db=session,
-            country_code=country_code,
-            official_id=contact.school_official_id
-        )
+        try:
+            school = crud.school.get_by_official_id_or_404(
+                db=session,
+                country_code=country_code,
+                official_id=contact.school_official_id
+            )
+        except:
+            print(f"Was told that {contact.school_official_id} was an official ID but couldn't find it?")
+            raise
     else:
-
         schools = crud.school.get_all_with_optional_filters(
             db=session,
             country_code=country_code,
             query_string=contact.school_name
         )
         if len(schools) == 1:
+            # We lucked out and found just one school with that name in this country.
             print(contact.id, schools[0].official_identifier)
-
             school = schools[0]
         elif len(schools) == 0:
-            print("Skipping missing school:")
-            print(contact)
-            print(schools)
-            # Let's make the school
+            if contact.country == "Australia":
+                raise SystemExit("Australian School Missing")
+            else:
+                # Let's make the USA schools later
+                unhandled_contacts.append(contact)
+                pass
             continue
         elif len(schools) > 1:
-            print("there were multiple schools found")
-            print(contact)
-            print(schools)
+            raise SystemExit("there were multiple schools found")
 
+    # If we made it here we have one school.
+    # Associate the user and the found school.
+    user.school = school
+    user.type = UserAccountType.LIBRARY
 
-    # Add the user to the school. Update the school with domains etc
-    school.users.append(user)
+    # Now mark the school as active
+    school.state = SchoolState.ACTIVE
 
+    # Update the school with any info from the contact string
 
+    session.commit()
+
+print()
+print("Non-Aus schools:")
+for i, contact in enumerate(unhandled_contacts, start=1):
+    print(i, contact.school_name, f"{contact.state_or_region}, {contact.country}")
