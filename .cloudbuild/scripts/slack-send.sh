2e68bba343237fdebf90d647345c33e0bbba1936
TheDen <deniskhoshaba@gmail.com>
2022-02-17 17:54:26 +1100

    fix URL

--- .cloudbuild/scripts/slack-send.sh
+++ .cloudbuild/scripts/slack-send.sh
@@ -2,7 +2,7 @@
 
 pr_number="$1"
 url_suffix="wriveted-api-development-lg5ntws4da-ts.a.run.app"
-deployment_url="https://${pr_number}---${url_suffix}"
+deployment_url="https://pr-${pr_number}---${url_suffix}"
 slack_message=":rocket: $(wriveted-api) [PR ${pr_number}]: <${deployment_url}|${deployment_url}> "
 
 curl -X POST \
