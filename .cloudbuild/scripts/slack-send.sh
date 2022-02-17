11553a5975b0167153c5d75d88871088d6690e5e
TheDen <deniskhoshaba@gmail.com>
2022-02-17 17:51:14 +1100

    refactor

--- .cloudbuild/scripts/slack-send.sh
+++ .cloudbuild/scripts/slack-send.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+pr_number="$1"
+url_suffix="wriveted-api-development-lg5ntws4da-ts.a.run.app"
+deployment_url="https://${pr_number}---${url_suffix}"
+slack_message=":rocket: $(wriveted-api) [PR ${_PR_NUMBER}]: <${deployment_url}|${deployment_url}> "
+
 curl -X POST \
   -H 'Content-type: application/json; charset=utf-8' \
-  --data "{ \"text\": \"pr-${_PR_NUMBER}---wriveted-api-development-lg5ntws4da-ts.a.run.app\" }" \
+  --data "{ \"text\": \"${slack_message}\" }" \
   "${SLACK_WEBHOOK}"
