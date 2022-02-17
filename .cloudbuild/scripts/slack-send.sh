4cc3692e6585ec4b4434d79ea5ebf16ebd979b90
TheDen <deniskhoshaba@gmail.com>
2022-02-17 18:31:18 +1100

    fix issue with escaping backticks

--- .cloudbuild/scripts/slack-send.sh
+++ .cloudbuild/scripts/slack-send.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 pr_number="$1"
 url_suffix="wriveted-api-development-lg5ntws4da-ts.a.run.app"
 deployment_url="https://pr-${pr_number}---${url_suffix}"
-slack_message=":rocket: $(wriveted-api) [<https://github.com/Wriveted/wriveted-api/pull/${pr_number}|PR ${pr_number}>]: <${deployment_url}|${deployment_url}> "
+slack_message=":rocket: \`wriveted-api\` [<https://github.com/Wriveted/wriveted-api/pull/${pr_number}|PR ${pr_number}>]: <${deployment_url}|${deployment_url}>"
 
 curl -X POST \
   -H 'Content-type: application/json; charset=utf-8' \
