64f1442e707fb078d6f5c085ad77ed3a198e954b
Joshua Landy <joshua@wriveted.com>
2022-02-20 18:13:17 +1100

    Add regex string

--- app/main.py
+++ app/main.py
@@ -95,6 +95,8 @@ if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        # allow preview channels from library dashboard app frontend
+        allow_origin_regex='https:\/\/wriveted-library--pr.*web\.app',
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
