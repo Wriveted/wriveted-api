14cd0406e35f74b95e4684484d2db5ed534106dd
TheDen <deniskhoshaba@gmail.com>
2022-03-03 15:41:24 +1100

    add integration tests to GitHub CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,18 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # Set bash to exit immediately on any command failure
 set -e
-SCRIPT_DIR=$(dirname "$0")
-
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-compose.yml down -v --remove-orphans
 docker-compose build --build-arg INSTALL_DEV=true
-
 docker-compose -f docker-compose.yml up -d
-
 docker-compose logs
 sleep 5
 
 # Now exec into the application to run migrations
-docker-compose exec -e SQLALCHEMY_DATABASE_URI=postgresql://postgres:xvc8kcn@db/postgres -T api bash /app/scripts/run-migrations.sh
+docker-compose exec -e SQLALCHEMY_DATABASE_URI=postgresql://postgres:xvc8kcn@db/postgres -T api bash /app/scripts/run-migrationss.h
 
 # Now start the integration tests
 docker-compose exec -T api bash /app/scripts/start-tests.sh "$@"
