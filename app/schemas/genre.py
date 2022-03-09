49435d431539f884e1f3b63510d553b9e350d31b
Joshua Landy <joshua@wriveted.com>
2022-03-09 20:02:01 +1100

    Added __eq__ and __hash__ to wrong class

--- app/schemas/genre.py
+++ app/schemas/genre.py
@@ -7,3 +7,11 @@ class Genre(BaseModel):
 
     class Config:
         orm_mode = True
+
+    def __eq__(self, other):
+        return self.name==other.name\
+            and self.source==other.source
+
+    def __hash__(self):
+        return hash(('name', self.name,
+            'source', self.source))
