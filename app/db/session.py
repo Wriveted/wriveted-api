d158651c5d40e517972afac06e557150ab61c571
Brian Thorne <brian@hardbyte.nz>
2022-03-13 06:23:50 +1300

    Configure the database connection pool to not reuse old connections (#51)

--- app/db/session.py
+++ app/db/session.py
@@ -14,8 +14,25 @@ from app.config import get_settings, Settings
 def database_connection(
     database_uri: str,
 ) -> Tuple[sqlalchemy.engine.Engine, sqlalchemy.orm.sessionmaker]:
-    # Ref: https://docs.sqlalchemy.org/en/13/core/pooling.html
-    engine = create_engine(database_uri)
+    # Ref: https://docs.sqlalchemy.org/en/14/core/pooling.html
+    engine = create_engine(
+        database_uri,
+        # Pool size is the maximum number of permanent connections to keep.
+        # defaults to 5
+        # pool_size=5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        # Default is 10
+        max_overflow=2,
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished on checkout.
+        # https://docs.sqlalchemy.org/en/14/core/pooling.html#setting-pool-recycle
+        pool_recycle=900,  # 15 minutes,
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        pool_timeout=30,
+    )
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     return engine, SessionLocal
