b40d259930269cc42794386907697bca3493d47f
TheDen <deniskhoshaba@gmail.com>
2022-03-18 21:14:19 +1100

    black fmt

--- scripts/manually_create_booklist.py
+++ scripts/manually_create_booklist.py
@@ -8,7 +8,16 @@ from sqlalchemy import distinct, func, select
 from sqlalchemy.orm import aliased
 
 from app.api.recommendations import get_recommended_editions_and_labelsets
-from app.models import BookList, CollectionItem, Edition, Hue, LabelSet, LabelSetHue, ReadingAbility, Work
+from app.models import (
+    BookList,
+    CollectionItem,
+    Edition,
+    Hue,
+    LabelSet,
+    LabelSetHue,
+    ReadingAbility,
+    Work,
+)
 from app.models.booklist import ListType
 from app.schemas.recommendations import ReadingAbilityKey
 
@@ -23,25 +32,34 @@ from app import config
 from app.db.session import get_session
 
 import logging
+
 logging.basicConfig()
-#logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 session = next(get_session(settings=config.get_settings()))
 
 works_for_booklist = []
 
-huey_picks_data = csv.reader(open('HueysPicks.csv', 'rt'))
+huey_picks_data = csv.reader(open("HueysPicks.csv", "rt"))
 next(huey_picks_data)
 for i, line in enumerate(huey_picks_data):
-    #print(line)
+    # print(line)
     title, isbns_str, author, scoutPicks = line
 
-    isbns = isbns_str.split(',')
+    isbns = isbns_str.split(",")
 
-    editions = session.execute(crud.edition.get_multi_query(db=session, ids=isbns)).scalars().all()
+    editions = (
+        session.execute(crud.edition.get_multi_query(db=session, ids=isbns))
+        .scalars()
+        .all()
+    )
 
     for edition in editions:
-        if edition is not None and edition.work is not None and edition.work not in works_for_booklist:
+        if (
+            edition is not None
+            and edition.work is not None
+            and edition.work not in works_for_booklist
+        ):
             works_for_booklist.append(edition.work)
             print(edition.work)
 
@@ -51,11 +69,7 @@ for i, line in enumerate(huey_picks_data):
 print(f"Found {len(works_for_booklist)} hydrated books")
 
 booklist_orm = BookList(
-    name="Huey's Picks",
-    type=ListType.HUEY_LIST,
-    info={},
-    works=works_for_booklist
+    name="Huey's Picks", type=ListType.HUEY_LIST, info={}, works=works_for_booklist
 )
 session.add(booklist_orm)
 session.commit()
-
