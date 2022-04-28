0fe78c26762ba03db60d339876d1860b9df7d3c8
Brian Thorne <brian@thorne.link>
2022-04-28 15:15:53 +1200

    ⚫

--- scripts/ingestion/aus/update-schools.py
+++ scripts/ingestion/aus/update-schools.py
@@ -10,40 +10,34 @@ from config import settings
 def get_all_aus_schools():
     schools_response = httpx.get(
         f"{settings.WRIVETED_API}/v1/schools",
-        headers={
-            'Authorization': f'Bearer {settings.WRIVETED_API_TOKEN}'
-        },
+        headers={"Authorization": f"Bearer {settings.WRIVETED_API_TOKEN}"},
         params={
             #'is_active': True,
-            'limit': 100_000,
-            'country_code': "AUS",
+            "limit": 100_000,
+            "country_code": "AUS",
         },
-        timeout=60
+        timeout=60,
     )
     return schools_response.json()
 
 
 print("Getting all Australian schools from Wriveted")
 aus_schools = get_all_aus_schools()
-schools_by_acara = {s['official_identifier']: s for s in aus_schools}
+schools_by_acara = {s["official_identifier"]: s for s in aus_schools}
 print("analyzing")
 
 
 def get_school_by_acara_id(acara_id):
     school = schools_by_acara[acara_id]
-    return school['wriveted_identifier'], school['info']
+    return school["wriveted_identifier"], school["info"]
 
 
 def update_wriveted_school_info(wriveted_id, info):
     response = httpx.put(
         f"{settings.WRIVETED_API}/v1/school/{wriveted_id}",
-        headers={
-            'Authorization': f'Bearer {settings.WRIVETED_API_TOKEN}'
-        },
-        json={
-            'info': info
-        },
-        timeout=10
+        headers={"Authorization": f"Bearer {settings.WRIVETED_API_TOKEN}"},
+        json={"info": info},
+        timeout=10,
     )
     if response.status_code != 200:
         print("Failed!")
@@ -67,9 +61,9 @@ pprint(raw_data[0])
 
 for school_data in raw_data:
     # 'ACARAId', 'SchoolURL', 'SchoolName'
-    school_name = school_data['SchoolName']
-    acara_ID = school_data['ACARAId']
-    school_url = school_data['SchoolURL']
+    school_name = school_data["SchoolName"]
+    acara_ID = school_data["ACARAId"]
+    school_url = school_data["SchoolURL"]
 
     try:
         wriveted_id, info = get_school_by_acara_id(acara_ID)
@@ -77,12 +71,11 @@ for school_data in raw_data:
         # Probably not an active school as we don't have them in the Wriveted DB
         continue
 
-    if info.get('URL') is None and len(school_url) > 0:
+    if info.get("URL") is None and len(school_url) > 0:
         print(f"<{school_name}> - {wriveted_id}")
         print(f"  {acara_ID} - {school_url}")
-        info['URL'] = school_url
+        info["URL"] = school_url
         update_wriveted_school_info(wriveted_id, info)
         time.sleep(0.5)
     else:
         print(f"Skipping {school_name}")
-
