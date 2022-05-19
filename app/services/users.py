f3cf3ee546d6d87383a4f5e1c063aa3f4da5d11f
Joshua Landy <joshua@wriveted.com>
2022-05-19 12:58:42 +1000

    Random usernames v2

--- app/services/users.py
+++ app/services/users.py
@@ -1,47 +1,62 @@
 import csv
+import os
 import random
 from structlog import get_logger
 from sqlalchemy.orm import Session
-
 from app import crud
 
 logger = get_logger()
 
 # default complexity: ColourNounNumber (RedWolf52)
-async def new_random_username(
+def new_random_username(
     session: Session,
     adjective: bool = False,
     color: bool = True,
     noun: bool = True,
-    numbers: int = 0,
+    numbers: int = 2,
 ):
     name = ""
     valid_name = False
 
-    with open("wordlist.csv") as f:
-        wordlist = csv.reader(f)
+    here = os.path.dirname(os.path.abspath(__file__))
+    # current csv capable of 11*11*11*99 ≈ 130k names
+    filename = os.path.join(here, "wordlist.csv")
+    with open(filename) as f:
+        data = csv.DictReader(f)
+        wordlist = list(data)
 
         while not valid_name:
-            name = generate_random_username_from_csv(
+            name = generate_random_username_from_wordlist(
                 wordlist, adjective, color, noun, numbers
             )
-            valid_name = name is not None and crud.user.get_by_username(name) is None
+            valid_name = (
+                name is not None and crud.user.get_by_username(session, name) is None
+            )
 
     return name
 
 
-# expects column headers 'adjective', 'colour', 'noun'
-def generate_random_username_from_csv(
-    csv, adjective: bool, colour: bool, noun: bool, numbers: int
+class WordListItem:
+    adjective: str
+    colour: str
+    noun: str
+
+
+def generate_random_username_from_wordlist(
+    wordlist: list[WordListItem],
+    adjective: bool,
+    colour: bool,
+    noun: bool,
+    numbers: int,
 ) -> str:
-    rows = list(csv)
     name = ""
+
     if adjective:
-        name = name + random.choice(rows)["adjective"]
+        name = name + random.choice(wordlist)["adjective"].title()
     if colour:
-        name = name + random.choice(rows)["colour"]
+        name = name + random.choice(wordlist)["colour"].title()
     if noun:
-        name = name + random.choice(rows)["noun"]
+        name = name + random.choice(wordlist)["noun"].title()
     if numbers:
         name = name + "".join([str(random.randint(0, 9)) for i in range(numbers)])
 
