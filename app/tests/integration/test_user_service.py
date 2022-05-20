04bab18953d72cc7e4017de01793243fae7cc835
Brian Thorne <brian@thorne.link>
2022-05-21 11:29:37 +1200

    Another test

--- app/tests/integration/test_user_service.py
+++ app/tests/integration/test_user_service.py
@@ -8,3 +8,15 @@ def test_generate_new_random_user_name(client, session):
         output = new_random_username(session=session, wordlist=wordlist)
 
     assert isinstance(output, str)
+
+
+def test_generate_random_user_name_from_fixed_list(client, session):
+    wordlist = [WordListItem(adjective="A", colour="C", noun="N")]
+
+    output = new_random_username(session=session, wordlist=wordlist)
+    assert isinstance(output, str)
+    assert output.startswith("CN")
+
+    output = new_random_username(session=session, wordlist=wordlist, adjective=True)
+    assert isinstance(output, str)
+    assert output.startswith("ACN")
