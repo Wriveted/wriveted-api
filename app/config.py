7a36ad5b9c36e008c89d7595ca1c0f66e3d5cdf0
Joshua Landy <97575656+jlandy-w@users.noreply.github.com>
2022-06-15 11:49:03 +1000

    add new cors origin for landbot

--- app/config.py
+++ app/config.py
@@ -87,6 +87,7 @@ class Settings(BaseSettings):
         "http://34.77.31.159",
         "http://23.251.142.192",
         "https://chats.landbot.io",
+        "https://landbot.site",
         # TypeBot
         "https://typebot.io",
         "http://13.38.101.232",
