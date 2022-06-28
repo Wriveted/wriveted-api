de8c927306d1789ae41c57bfb8ed2acb7a87a0d3
Joshua Landy <joshua@wriveted.com>
2022-06-28 17:41:51 +1000

    fix schema datetimes for json blobs

--- app/schemas/users/huey_attributes.py
+++ app/schemas/users/huey_attributes.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 from app.schemas.recommendations import ReadingAbilityKey
 
 
 class HueyAttributes(BaseModel):
-    birthdate: datetime | None
+    birthdate: constr(regex=r'(\d{4})-(\d{2})-(\d{2})( (\d{2}):(\d{2}):(\d{2}))?') | None
     reading_ability: list[ReadingAbilityKey] | None
-    last_visited: datetime | None
+    last_visited: constr(regex=r'(\d{4})-(\d{2})-(\d{2})( (\d{2}):(\d{2}):(\d{2}))?') | None
