52f2793e163bb280f62fe4d0bbb908383654c1ad
Joshua Landy <joshua@wriveted.com>
2022-08-16 12:44:40 +1000

    debug printing

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -3,17 +3,21 @@
 # Set bash to exit immediately on any command failure
 set -e
 
-# use test env
-source .test-env
-
 # Remove possibly previous broken stacks left hanging after an error
+echo "docker-compose -f docker-compose.yml down -v --remove-orphans"
 docker-compose -f docker-compose.yml down -v --remove-orphans
+echo "docker-compose build --build-arg INSTALL_DEV=true"
 docker-compose build --build-arg INSTALL_DEV=true
 
 docker-compose -f docker-compose.yml up -d
+echo "docker-compose -f docker-compose.yml up -d"
 docker-compose logs
+echo "docker-compose logs"
 sleep 5
+echo "sleep 5"
 
 # Now start the integration tests
+echo "docker-compose exec -T api python app/db/check_can_connect_to_db.py"
 docker-compose exec -T api python app/db/check_can_connect_to_db.py
+echo 'docker-compose exec -T api bash /app/scripts/start-tests.sh' $*
 docker-compose exec -T api bash /app/scripts/start-tests.sh "$@"
