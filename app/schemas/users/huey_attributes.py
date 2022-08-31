80ed620525d601b907df1153b231334979d1dde5
Joshua Landy <joshua@wriveted.com>
2022-08-31 15:05:17 +1000

    add more huey attributes to readers

--- app/schemas/users/huey_attributes.py
+++ app/schemas/users/huey_attributes.py
@@ -2,14 +2,21 @@ from __future__ import annotations
 
 from pydantic import BaseModel, constr
 
-from app.schemas.recommendations import ReadingAbilityKey
+from app.schemas.recommendations import ReadingAbilityKey, HueKeys
 
 
 class HueyAttributes(BaseModel):
     birthdate: constr(
         regex=r"(\d{4})-(\d{2})-(\d{2})( (\d{2}):(\d{2}):(\d{2}))?"
     ) | None
-    reading_ability: list[ReadingAbilityKey] | None
     last_visited: constr(
         regex=r"(\d{4})-(\d{2})-(\d{2})( (\d{2}):(\d{2}):(\d{2}))?"
     ) | None
+
+    age: int | None
+    reading_ability: list[ReadingAbilityKey] | None
+    hues: list[HueKeys] | None
+
+    goals: list[str] | None
+    genres: list[str] | None
+    characters: list[str] | None
