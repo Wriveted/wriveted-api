3fa54d3420981feaaac3deb31935ecd0d90096ef
Joshua Landy <joshua@wriveted.com>
2022-09-11 13:53:34 +1000

    add cors

--- app/main.py
+++ app/main.py
@@ -112,7 +112,7 @@ if settings.BACKEND_CORS_ORIGINS:
         CORSMiddleware,
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         # allow preview channels from library dashboard app frontend
-        allow_origin_regex="https:\/\/wriveted.*web\.app",
+        allow_origin_regex="(https:\/\/wriveted.*web\.app)|(https:\/\/huey-books.*web\.app)",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
