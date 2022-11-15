8020380d99fe9595f42b834e11d3ab5eb11b2529
Joshua Landy <joshua@wriveted.com>
2022-11-15 17:28:40 +1100

    canonical isbn in edition dependency

--- app/api/dependencies/editions.py
+++ app/api/dependencies/editions.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 
 from app import crud
 from app.db.session import get_session
+from app.services.editions import get_definitive_isbn
 
 
 def get_edition_from_isbn(
     isbn: str = Path(..., description="ISBN string representing a unique edition"),
     session: Session = Depends(get_session),
 ):
-    return crud.edition.get_or_404(db=session, id=isbn)
+    return crud.edition.get_or_404(db=session, id=get_definitive_isbn(isbn))
