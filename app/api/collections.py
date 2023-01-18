ca51bf7bc301d9ec5a146733d1d087897753f144
Joshua Landy <joshua@wriveted.com>
2023-01-18 22:04:25 +1100

    re-catch pydantic validation in endpoint

--- app/api/collections.py
+++ app/api/collections.py
@@ -9,6 +9,7 @@ from fastapi import (
     Request,
     Security,
 )
+from pydantic import ValidationError
 from sqlalchemy import delete, func, select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -470,9 +471,18 @@ async def log_collection_item_activity(
     Create new activity entry for a collection item and reader
     """
     # Since we have already consumed parts of the request body in the permission checks,
-    # we need to re-read the body here (using the Request object) to get the data again
+    # we need to re-read the body here (using the Request object) to get the data again.
+    # (there is a choice here between having to re-read the body, or having to
+    # recreate the permission checks in the body of the function.
+    # the former was opted as it is one reconstruction vs two)
     body = await req.json()
-    collection_item_activity_data = CollectionItemActivityBase(**body)
+    try:
+        collection_item_activity_data = CollectionItemActivityBase(**body)
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=422,
+            detail=e.json(),
+        )
 
     logger.info(
         "Creating collection item activity",
