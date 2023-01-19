897d977edd4686fc821d4ddab5ac1c17a39be076
Joshua Landy <joshua@wriveted.com>
2023-01-19 14:14:41 +1100

    add negative collectionitemactivity tests

--- app/tests/integration/test_collection_item_activity.py
+++ app/tests/integration/test_collection_item_activity.py
@@ -1,11 +1,17 @@
+from app.api.dependencies.security import create_user_access_token
 from app.models.collection_item_activity import CollectionItemReadStatus
+from app.schemas.users.user_create import UserCreateIn
+from app import crud
 
 
 def test_collection_item_activity_creation_and_fetching(
+    session,
     client,
     test_unhydrated_editions,
     test_user_account,
     test_user_account_headers,
+    test_public_user_hacker,
+    test_public_user_hacker_headers,
 ):
     test_user_id = str(test_user_account.id)
 
@@ -88,3 +94,41 @@ def test_collection_item_activity_creation_and_fetching(
     )
     assert len(collection_items_interacted_with_data["data"]) == 1
     assert collection_items_interacted_with_data["data"][0]["id"] == collection_item_id
+
+    # Hacker to test permissions
+
+    print("Testing auth/me endpoint for hacker")
+
+    hacker_id = str(test_public_user_hacker.id)
+
+    print("Attempting to create collection item activity for another user")
+    hacker_collection_item_activity_create_response = client.post(
+        f"/v1/collection-item-activity",
+        headers=test_public_user_hacker_headers,
+        json={
+            "reader_id": test_user_id,
+            "collection_item_id": collection_item_id,
+            "status": CollectionItemReadStatus.READING,
+        },
+    )
+    assert hacker_collection_item_activity_create_response.status_code == 403
+
+    print("Attempting to query for collection items being read by another user")
+    hacker_collection_items_being_read_response = client.get(
+        f"/v1/collection/{user_collection_id}/items",
+        headers=test_public_user_hacker_headers,
+        params={"reader_id": test_user_id, "read_status": "READING"},
+    )
+    assert hacker_collection_items_being_read_response.status_code == 403
+
+    print("Attempting to add activity on a collection item from another user")
+    hacker_collection_item_activity_create_response = client.post(
+        f"/v1/collection-item-activity",
+        headers=test_public_user_hacker_headers,
+        json={
+            "reader_id": hacker_id,
+            "collection_item_id": collection_item_id,
+            "status": CollectionItemReadStatus.READING,
+        },
+    )
+    assert hacker_collection_item_activity_create_response.status_code == 403
