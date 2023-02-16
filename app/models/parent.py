1d5e3c4786f0aee7fffc60f48ff5a4b88f74483d
Joshua Landy <joshua@wriveted.com>
2023-02-16 18:49:41 +1100

    add readers and their supporters to parent model

--- app/models/parent.py
+++ app/models/parent.py
@@ -1,8 +1,10 @@
 from fastapi_permissions import All, Allow
 from sqlalchemy import JSON, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
+import uuid
 from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.orm import mapped_column, relationship, Mapped
+from app.models.subscription import Subscription
 
 from app.models.user import User, UserAccountType
 
@@ -12,7 +14,7 @@ class Parent(User):
     A concrete Parent of Students or PublicReaders.
     """
 
-    id = mapped_column(
+    id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
         ForeignKey("users.id", name="fk_parent_inherits_user", ondelete="CASCADE"),
         primary_key=True,
@@ -20,22 +22,26 @@ class Parent(User):
 
     __mapper_args__ = {"polymorphic_identity": UserAccountType.PARENT}
 
-    subscription = relationship(
+    subscription: Mapped[Subscription | None] = relationship(
         "Subscription",
         back_populates="parent",
         uselist=False,
         cascade="all, delete-orphan",
     )
 
-    # readers = relationship(
-    #     "Reader",
-    #     back_populates="parent",
-    # )
+    readers = relationship(
+        "Reader",
+        back_populates="parent",
+    )
 
     reader_supporters = relationship(
         "Supporter",
         back_populates="parent",
     )
+
+    # @property
+    # def reader_supporters(self):
+    #     return [supporter for reader in self.readers for supporter in reader.supporters]
 
     # misc
     parent_info = mapped_column(MutableDict.as_mutable(JSON), nullable=True, default={})
