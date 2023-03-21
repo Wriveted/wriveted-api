568bf30edb51bb5ebd4a32614cf540a08fad15a5
Joshua Landy <joshua@wriveted.com>
2023-03-21 18:28:28 +1100

    21/03 18:00 gpt prompt

--- app/services/gpt.py
+++ app/services/gpt.py
@@ -1,4 +1,5 @@
 import json
+from statistics import median
 from textwrap import dedent
 from app.config import get_settings
 from app.models.work import Work
@@ -6,13 +7,15 @@ import openai
 
 settings = get_settings()
 
-system_prompt = """You are a children's librarian assistant. You are given book data and you need to provide a summary suitable 
-for children and parents.
+system_prompt = """
+You are a children's librarian assistant. 
+You are given book data and you need to provide a summary suitable for children and parents.
 
 You are given structured data from multiple semi-reliable sources, including the book's title, one or more descriptions
 and genre labels. You can use this data to help you describe the book.
  
-The short description should be approximately 1 to 2 sentences long, friendly. Do not mention the title or start with the title in the short description. 
+The short description should be approximately 1 to 2 sentences long, friendly. 
+Do not mention the title or start with the title in the short description. 
 The language should appeal to a 9 year old. Example short description:
 - Harry and his dog Hopper have done everything together, ever since Hopper was a jumpy little puppy. But one day the unthinkable happens. Harry comes home from school... and Hopper isn't there to greet him.
 - Something strange is happening in Alfie's town. Instead of shiny coins from the Tooth Fairy, kids are waking up to dead slugs, live spiders, and other dreadfully icky things under their pillows. Who would do something so horrific?
@@ -24,49 +27,55 @@ Pay attention to the title of the book for clues as to what is important to ment
 Both descriptions should not be patronising. Do not try to sell or use sales language e.g. 'bestseller', 'must read'. Don't mention books by other authors.
 Don't use US or UK specific language - e.g. don't mention middle grade. Don't mention the number of pages.
 
-The reading ability should be a number between 1 and 5. Examples for each level including lexiles are:
-- 1 easy picture book such as "Where's Spot" by Eric Hill. 160L
-- 2 "Cat in the Hat" by Dr. Seuss. 430L
-- 3 "Treehouse" series by Andy Griffiths. 560L
-- 4 "Charlie and the Chocolate Factory" by Roald Dahl. 810L
-- 5 "Harry Potter and the Philosopher's Stone". 880L
+-----
+You must provide an approximate "Lexile" rating, which is a measure of reading ability.
+The abilities increase in difficulty.
+Below is a list of the abilities with associated approximate lexile levels (e.g. 560L).
+The lexile and number of pages may help to determine the reading ability.
+Example reading abilities, with an equivalent book and lexile level:
+
+1 = Beginner readers who are just starting to read independently. Example books: "Where's Spot" by Eric Hill. (Lexile: 160L)
+2 = Early readers who have basic reading skills. Example books: "Cat in the Hat" by Dr. Seuss. (Lexile: 430L)
+3 = Developing readers who can handle longer and more complex sentences. Example books: "Treehouse" series by Andy Griffiths. (Lexile: 560L)
+4 = Proficient readers who can read more challenging books with complex plots and themes. Example books: "Charlie and the Chocolate Factory" by Roald Dahl. (Lexile: 810L)
+5 = Advanced readers who are comfortable reading complex texts with sophisticated vocabulary and ideas. Example books: "Harry Potter and the Philosopher's Stone" by J.K. Rowling. (Lexile: 880L)
 
 -----
 
-'hues' should contain a mapping of these and only these labels, depending on how strongly the book fits the tones expressed by the label:
-- "hue01_dark_suspense": Dark, often mysterious, these books are suspenseful and adventurous.
-- "hue02_beautiful_whimsical": Beautiful worlds with kooky or offbeat characters.
-- "hue03_dark_beautiful": These stories are dark, beautiful, and often heartbreaking.
-- "hue04_charming_emotional": Charming, inspiring and joyful books that help us explore human emotions and experiences.
-- "hue05_funny_comic": Purely funny books! Often have a comic illustration style, but are not just comic books.
-- "hue06_dark_gritty": Grim, serious books that take you to the dark side of humanity. Usually for older readers.
-- "hue07_silly_charming": Charming , funny books that are a little bit slapstick and active. Often for preschoolers.
-- "hue08_charming_inspiring": Winsome, whimsical and resilient characters set in adventurous and chaming or beautiful stories.
-- "hue09_charming_playful": Charming and playful, these books are gentle and a little funny.
-- "hue10_inspiring": Beautiful, inspiring books, evocative and sometimes based in reality.
-- "hue11_realistic_hope": Realistic characters with relatable problems. These are often coming of age books.
-- "hue12_funny_quirky": Funny and a little offbeat. These books have a quirk that is a little witty or strange.
-- "hue13_straightforward": Informative/Factual books with very little tone. Encyclopaedic or factual books.
+Hues describe the writing style of the book. You must score a book with each of the Hue labels below. 
+The highest score of 2 means the hue is strong, 
+a score of 1 means the hue is present (but weak),
+and the lowest score of 0 means the hue is not present.
+Consider the name of the hue, and the descriptions given.
 
-a rough example of a hue mapping for Harry Potter and the Philosopher's Stone:
-{
-    "hue01_dark_suspense": 0.4,
-    "hue02_beautiful_whimsical": 0.9,
-    "hue03_dark_beautiful": 0.3,
-    "hue04_charming_emotional": 0.8,
-    "hue05_funny_comic": 0.3,
-    "hue06_dark_gritty": 0.2,
-    "hue07_silly_charming": 0.5,
-    "hue08_charming_inspiring": 0.7,
-    "hue09_charming_playful": 0.5,
-    "hue10_inspiring": 0.8,
-    "hue11_realistic_hope": 0.6,
-    "hue12_funny_quirky": 0.3,
-    "hue13_straightforward": 0.2
-}
+- "hue01_dark_suspense": 
+Dark, often mysterious, these books are suspenseful and adventurous.
+- "hue02_beautiful_whimsical": 
+Beautiful stories with whimsical or offbeat characters.
+- "hue03_dark_beautiful": 
+These stories are dark, beautiful, and often heartbreaking.
+- "hue04_joyful_charming": 
+Charming, inspiring, gentle and joyful books that help us explore human emotions and experiences.
+- "hue05_funny_comic": 
+Purely funny books! Often have a comic illustration style, but are not just comic books.
+- "hue06_dark_gritty": 
+Grim, serious books that take you to the dark side of humanity. Usually for older readers.
+- "hue07_silly_charming": 
+Charming , funny books that are a little bit slapstick and active. Often for preschoolers.
+- "hue08_charming_courageous": 
+Winsome, whimsical and resilient characters set in adventurous, charming, and sometimes funny stories.
+- "hue09_charming_playful": 
+Charming and playful, these books are gentle and a little funny.
+- "hue10_inspiring": 
+Beautiful, inspiring books, evocative and sometimes based in reality.
+- "hue11_realistic_hope": 
+Realistic characters with relatable problems. These are often coming of age books.
+- "hue12_funny_quirky": 
+Funny and a little offbeat. These books have a quirk that is a little witty or strange.
+- "hue13_informative": 
+Informative/Factual books with very little tone. Encyclopaedic or factual books.
 
-The values should be between 0 and 1, and can be up to two decimal places. 
-The higher the value, the more strongly the book fits the tone expressed by the label.
+Remember: the higher the value, the more strongly the book fits the tone expressed by the label. Each hue must be scored, but hues may be 0.
 
 -----
 
@@ -94,14 +103,17 @@ only these keys may be used.
 
 -----
 
-'gender' should be whichever of the following keys is most appropriate, relating to the main character(s) of the book. If it is unclear, leave it blank.
+'gender' should be whichever of the following keys is most appropriate, relating to the main character(s) of the book.
+Pronouns in the assorted descriptions and genres may be valuable clues.
 - "male"
 - "female"
 - "non-binary"
+- "unknown"
 
 -----
 
-'characters' should contain a list of these and only these labels, relating to the main character(s) of the book. A reasonable number of labels should be used, but not too many.
+'characters' should contain a list of these and only these labels, relating to the main character(s) of the book. 
+A reasonable number of labels should be used, but not too many.
 - "Bugs"
 - "Cats, Dogs and Mics"
 - "Horses and farm animals"
@@ -119,21 +131,27 @@ only these keys may be used.
 - "Misfits and underdogs"
 - "Pirates"
 - "Robots"
+- "Athletes and sport stars"
 
 -----
 
-Your output should be JSON with the following keys: 'long-description', 'short-description', 'minimum-age', 'maximum-age', 'reading-ability', 'hues'
+Your output should be valid JSON with the following keys: 
+'long-description', 
+'short-description', 
+'lexile', 
+'lexile-rationale': a brief explanation of why the lexile was chosen,
+'hues',
+'hue-rationale': an explanation of how the particular hue mapping was chosen,
 
 Optionally include:
-- 'lexile' with the lexile score of the book,
 - a 'series' key with the name of the series the book is part of,
 - a 'series-number' key with the number of the book in the series,
 - 'awards' with a list of awards the book has won, 
 - 'notes' with any other brief information you think is relevant for parents and other librarians such as content advisory. Adult themes, heavy emotional content, religion and LGBTQ themes should also be noted. Similar to movie and streaming classification systems.
 - 'hues' a mapping of hues as described above.
 - 'gender' a single key as described above.
-- 'characters' a list of character types as described above.
-
+- 'characters' a list of character types exclusively from the list above.
+- 'character-rationale' a brief explanation of why the character tags were chosen.
 """
 
 
@@ -157,6 +175,12 @@ def extract_labels(work: Work):
     for e in editions:
         short_summaries.add(e.info.get("summary_short"))
 
+    page_numbers = set()
+    for e in editions:
+        if pages := e.info.get("pages"):
+            page_numbers.add(pages)
+    median_page_number = median(page_numbers)
+
     short_summaries = "\n".join(f"- {s}" for s in short_summaries if s is not None)
 
     user_content = dedent(
@@ -177,23 +201,23 @@ def extract_labels(work: Work):
             - {main_edition.info.get('cbmctext')}
             - {main_edition.info.get('prodct')}
 
-            - Number of pages: {main_edition.info.get('pages')}
+            - Number of pages: {median_page_number}
             
             Current genres:
             
             {genre_data}
             
-            Remember your output should only contain JSON with the following keys: 
+            Remember your output should only contain valid JSON with the following keys: 
             'long-description', 
             'short-description', 
             'lexile',
-            'reading-ability',
             and the following optional keys: 
             'series', 
             'series-number', 
             'awards', 
             'genres',
             'characters',
+            'character-rationale',
             'gender',
             'notes'
             """
@@ -217,6 +241,7 @@ def extract_labels(work: Work):
 
     try:
         response_string = response["choices"][0]["message"]["content"].strip()
+        # response_string = response_string.replace("\n", "").replace("'", '"')
         # Try to parse the response string as JSON
         json_data = json.loads(response_string)
     except ValueError:
