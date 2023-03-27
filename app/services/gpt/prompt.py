834d64b3874c121184c8cc073fc7dac333c5155c
Joshua Landy <joshua@wriveted.com>
2023-03-27 17:53:52 +1100

    add fairytales genre

--- app/services/gpt/prompt.py
+++ app/services/gpt/prompt.py
@@ -260,5 +260,4 @@ Here is a validation error message encountered when parsing:
 ------------------------
 
 Please re-generate the requested data, ensuring that it matches the expected format.
-=======
 """
