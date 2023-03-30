7c693d6463097a276172f724746a53c52ddcb3f9
Brian Thorne <brian@thorne.link>
2023-03-30 15:41:35 +1300

    Add fuzzystrmatch via alembic

---
+++ alembic/versions/671ac58d1a3d_add_fuzzystrmatch.py
@@ -0,0 +1,29 @@
+"""noop
+
+Revision ID: 671ac58d1a3d
+Revises: 621df72d3942
+Create Date: 2023-03-30 15:33:00.162016
+
+"""
+import sqlalchemy as sa
+from alembic_utils.pg_extension import PGExtension
+from alembic_utils.pg_grant_table import PGGrantTable
+from sqlalchemy import text as sql_text
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "671ac58d1a3d"
+down_revision = "621df72d3942"
+branch_labels = None
+depends_on = None
+
+
+def downgrade():
+    public_fuzzystrmatch = PGExtension(schema="public", signature="fuzzystrmatch")
+    op.drop_entity(public_fuzzystrmatch)
+
+
+def upgrade():
+    public_fuzzystrmatch = PGExtension(schema="public", signature="fuzzystrmatch")
+    op.create_entity(public_fuzzystrmatch)
