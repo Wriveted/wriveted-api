ca00bd8d6b1210d6138bfdb49203488f86048bcc
Joshua Landy <joshua@wriveted.com>
2023-04-03 18:28:37 +1000

    list of failed gpt books, fix sqlalchemy weirdness

--- scripts/gpt_labeller.py
+++ scripts/gpt_labeller.py
@@ -1,17 +1,19 @@
 import asyncio
-from sqlalchemy import and_, or_, select
-from sqlalchemy.orm import contains_eager
-from sqlalchemy.orm import Session
+from sqlalchemy import and_, exists, or_, select
+from sqlalchemy.orm import contains_eager, Session
 from structlog import get_logger
 
 from app.config import get_settings
-from app.crud import event as crud_event
+from app.crud import event as crud_event, booklist as crud_booklist
 from app.db.session import database_connection
 from app.models import Edition
+from app.models.booklist import BookList
+from app.models.booklist_work_association import BookListItem
 from app.models.collection import Collection
 from app.models.collection_item import CollectionItem
 from app.models.labelset import LabelSet
 from app.models.work import Work
+from app.schemas.booklist import BookListUpdateIn, ItemUpdateType
 
 # from app.services.events import create_event
 from app.services.gpt import label_and_update_work
@@ -36,17 +38,11 @@ school_ids = [
     "be63175e-b81e-4f2b-a853-5d9ed0cf0e57",
     "851b23be-7b17-4b59-bd59-2f3765ad753c",
     "b8a80b9e-cd71-409e-8a70-76f758e1d157",
-    "aa1d9753-3975-4912-b0eb-62ad64ad7e1a",
-    "416f3908-bd7f-42c8-ac2c-58eb26339d48",
-    "70759ce9-f139-4bab-b396-16835d0d92a6",
-    "9733579e-452d-4a2b-b677-dece8f87ee21",
-    "334929c2-c031-4a8f-8da9-03f749ab6b3e",
-    "fb0919d9-4510-46a1-87e9-1ab2c27c91cf",
     "b384ffd1-36ad-4fb9-9dfd-4277453ff903",
-    "2546a8c8-0015-4059-98bc-dccfb3c9cfb0",
     "cbe0280b-5350-4a32-b524-ef13043d1db8",
-    "99928227-466f-4d33-9a1e-ce37eaa033d6",
 ]
+
+failed_list_id = "3f56747b-f008-4394-b571-0ae4f4c1c339"
 
 
 async def label():
@@ -61,7 +57,7 @@ async def label():
             work = (
                 session.query(Work)
                 .join(Work.labelset)
-                .join(Edition)
+                .join(Edition, isouter=False)
                 .join(
                     CollectionItem,
                     and_(
@@ -83,14 +79,25 @@ async def label():
                         ~LabelSet.hues.any(),
                     ),
                     Work.editions.any(Edition.cover_url.isnot(None)),
+                    ~exists().where(
+                        and_(
+                            BookListItem.work_id == Work.id,
+                            BookListItem.booklist_id == failed_list_id,
+                        )
+                    ),
                 )
                 .order_by(Edition.date_published.desc())
                 .options(
                     contains_eager(Work.editions).contains_eager(Edition.collections)
                 )
                 .limit(1)
-                .one_or_none()
+                .one()
             )
+
+            # to avoid lazy loading and caching weirdness, we need to expunge the work and re-fetch it
+            work_id = work.id
+            session.expunge(work)
+            work = session.get(Work, work_id)
 
             try:
                 await label_and_update_work(work, session)
@@ -101,6 +108,12 @@ async def label():
                     f"Failed to label {work.title}.",
                     {"work_id": work.id, "error": str(e)},
                 )
+
+                failed_list = session.query(BookList).get(failed_list_id)
+                update = BookListUpdateIn(
+                    items=[{"work_id": work.id, "action": ItemUpdateType.ADD}]
+                )
+                crud_booklist.update(db=session, db_obj=failed_list, obj_in=update)
 
                 logger.warning(f"Failed to label {work.title}. Skipping...")
                 continue
@@ -115,3 +128,33 @@ async def main():
 
 
 asyncio.run(main())
+
+# to get the number of works that need to be labeled:
+# with Session(engine) as session:
+#   work_count = (
+#     session.query(func.count(distinct(Work.id)))
+#     .join(Work.labelset)
+#     .join(Edition)
+#     .join(
+#         CollectionItem,
+#         and_(
+#             Edition.isbn == CollectionItem.edition_isbn,
+#             CollectionItem.collection_id.in_(
+#                 select(Collection.id).where(Collection.school_id.in_(school_ids))
+#             ),
+#         ),
+#         isouter=True,
+#     )
+#     .join(Collection)
+#     .filter(
+#         LabelSet.min_age.isnot(None),
+#         LabelSet.max_age.isnot(None),
+#         or_(
+#             ~LabelSet.reading_abilities.any(),
+#             ~LabelSet.hues.any(),
+#         ),
+#         Work.editions.any(Edition.cover_url.isnot(None)),
+#     )
+#     .one()[0]
+#   )
+#   print(work_count)
