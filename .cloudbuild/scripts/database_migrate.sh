2369342580013d752a6328ff42de1183ab8195f9
TheDen <deniskhoshaba@gmail.com>
2023-05-04 10:13:09 +1000

    replace wget with curl

--- .cloudbuild/scripts/database_migrate.sh
+++ .cloudbuild/scripts/database_migrate.sh
@@ -13,7 +13,7 @@ proxy_connection_cleanup() {
 trap proxy_connection_cleanup EXIT SIGTERM SIGINT SIGQUIT
 
 echo "Downloading cloud_sql_proxy"
-wget -q "https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64" -O cloud_sql_proxy
+curl -s "https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64" -o cloud_sql_proxy
 chmod +x cloud_sql_proxy
 ./cloud_sql_proxy -instances="${CLOUD_SQL_INSTANCE}=tcp:localhost:${POSTGRES_PORT}" &
 
