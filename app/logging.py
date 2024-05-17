e3cd13cefcc3538d902e1504ef0d7700282ce98c
Brian Thorne <brian@thorne.link>
2024-05-18 09:59:37 +1200

    Revert custom trace around recommendation

--- app/logging.py
+++ app/logging.py
@@ -64,7 +64,7 @@ def init_logging(settings: Settings):
         structlog.contextvars.merge_contextvars,
         structlog.stdlib.add_logger_name,
         structlog.stdlib.add_log_level,
-        add_open_telemetry_spans,
+        # add_open_telemetry_spans,
         # Don't need a timestamp as cloudrun already adds one
         # structlog.processors.TimeStamper(fmt='iso'),
         structlog.processors.StackInfoRenderer(),
