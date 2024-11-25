18891ffcf5edd7f724ea0e962585ca57fb7da7a4
Brian Thorne <brian@thorne.link>
2024-11-25 20:57:36 +1300

    🐳 Compose now part of docker

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -15,14 +15,14 @@ else
 fi
 
 # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-compose.yml down -v --remove-orphans
-docker-compose build --build-arg INSTALL_DEV=true --build-arg BUILDKIT_INLINE_CACHE=1
-docker-compose -f docker-compose.yml up -d db migration
-docker-compose logs migration
+docker compose -f docker-compose.yml down -v --remove-orphans
+docker compose build --build-arg INSTALL_DEV=true --build-arg BUILDKIT_INLINE_CACHE=1
+docker compose -f docker-compose.yml up -d db migration
+docker compose logs migration
 sleep 5
 
-docker-compose -f docker-compose.yml up -d api internal
+docker compose -f docker-compose.yml up -d api internal
 
 # Now start the integration tests
-docker-compose exec -T api python app/db/check_can_connect_to_db.py
-docker-compose exec -T api bash /app/scripts/start-tests.sh "$@"
+docker compose exec -T api python app/db/check_can_connect_to_db.py
+docker compose exec -T api bash /app/scripts/start-tests.sh "$@"
