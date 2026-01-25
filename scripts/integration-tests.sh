d1dfb70dbbbad2fbd763df2589f37786131dfbd3
Brian Thorne <brian@thorne.link>
2026-01-25 13:51:57 +1300

    Reset async DB pools in tests

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -23,8 +23,6 @@ docker compose -f docker-compose.yml up -d db migration
 docker compose logs migration
 sleep 5
 
-docker compose -f docker-compose.yml up -d api internal
-
 # Now start the integration tests
-docker compose exec -T api python app/db/check_can_connect_to_db.py
-docker compose exec -T api bash /app/scripts/start-tests.sh "$@"
+docker compose -f docker-compose.yml run --rm --entrypoint python api app/db/check_can_connect_to_db.py
+docker compose -f docker-compose.yml run --rm --entrypoint bash api /app/scripts/start-tests.sh "$@"
